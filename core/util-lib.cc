ea6dd506f55602e2358927cd4ce536b8346a4d3f
Themis Bourdenas <bourdenas@gmail.com>
2018-03-02 22:33:29 +0100

    Change Vector value type from int to double and add basic Vec operations

--- core/util-lib.cc
+++ core/util-lib.cc
@@ -1,5 +1,7 @@
 #include "core/util-lib.h"
 
+#include <math.h>
+
 #include "core/resource-manager.h"
 #include "proto/sprite.pb.h"
 
@@ -15,5 +17,45 @@ Box GetSceneNodeBoundingBox(const SceneNode& node) {
   return bounding_box;
 }
 
+double VectorLength(const Vector& v) {
+  return std::sqrt(VectorSquaredLength(v));
+}
+
+double VectorSquaredLength(const Vector& v) {
+  return v.x() * v.x() + v.y() * v.y() + v.z() * v.z();
+}
+
+void VectorNormalise(Vector* v) {
+  const auto len = VectorLength(*v);
+  if (len > 1e-08) {
+    *v = *v * (1.0f / len);
+  }
+}
+
 }  // namespace util
+
+Vector operator+(const Vector& left, const Vector& right) {
+  Vector v;
+  v.set_x(left.x() + right.x());
+  v.set_y(left.y() + right.y());
+  v.set_z(left.z() + right.z());
+  return v;
+}
+
+Vector operator-(const Vector& left, const Vector& right) {
+  Vector v;
+  v.set_x(left.x() - right.x());
+  v.set_y(left.y() - right.y());
+  v.set_z(left.z() - right.z());
+  return v;
+}
+
+Vector operator*(const Vector& v, double a) {
+  Vector u;
+  u.set_x(v.x() * a);
+  u.set_y(v.y() * a);
+  u.set_z(v.z() * a);
+  return u;
+}
+
 }  // namespace troll
--- core/util-lib.h
+++ core/util-lib.h
@@ -9,7 +9,17 @@ namespace util {
 
 Box GetSceneNodeBoundingBox(const SceneNode& node);
 
+double VectorLength(const Vector& v);
+double VectorSquaredLength(const Vector& v);
+
+void VectorNormalise(Vector* v);
+
 }  // namespace util
+
+Vector operator+(const Vector& left, const Vector& right);
+Vector operator-(const Vector& left, const Vector& right);
+Vector operator*(const Vector& v, double a);
+
 }  // namespace troll
 
 #endif  // TROLL_CORE_UTIL_LIB_H_
