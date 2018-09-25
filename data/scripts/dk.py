bd10c8ba050ea136647219871a7259bbcc562c6c
Unknown <bourdenas@gmail.com>
2018-09-25 16:15:36 +0200

    Lint python files
    with flake8 and small changes in the example script.

--- data/scripts/dk.py
+++ data/scripts/dk.py
@@ -4,34 +4,35 @@ import troll
 
 
 def init():
-  objects = [
-      sprites.Mario('mario'), sprites.Mario('mario_clone'),
-      sprites.Mario('third_mario'),
-  ]
+    objects = [
+        sprites.Mario('mario'), sprites.Mario('mario_clone'),
+        sprites.Mario('third_mario'),
+    ]
 
-  objects[0].Create(15, (100, 120))
-  objects[0].PlayAnimation('mario_walk_right')
-  objects[1].Create(0, (300, 60))
-  objects[2].Create(0, (500, 120))
-  objects[2].PlayAnimation('mario_walk_left')
+    objects[0].Create(15, (100, 120))
+    objects[0].PlayAnimation('mario_walk_right')
+    objects[1].Create(0, (300, 60))
+    objects[2].Create(0, (500, 120))
+    objects[2].PlayAnimation('mario_patrol')
 
-  dk = sprites.DonkeyKong('dk')
-  dk.Create(0, (100, 200))
-  dk.PlayAnimation('dk_taunt')
+    dk = sprites.DonkeyKong('dk')
+    dk.Create(0, (100, 200))
+    dk.PlayAnimation('dk_taunt')
 
-  platform_sizes = [3, 13, 13, 13, 13, 13, 14]
-  platforms = [sprites.Platform('platform_' + str(i), size)
-               for i, size in enumerate(platform_sizes)]
-  platform_positions = [
-    (280, 97), (80, 155), (112, 210), (80, 275), (112, 335), (80, 395),
-    (80, 455),
-  ]
+    platform_sizes = [3, 13, 13, 13, 13, 13, 14]
+    platforms = [sprites.Platform('platform_' + str(i), size)
+                 for i, size in enumerate(platform_sizes)]
+    platform_positions = [
+        (280, 97), (80, 155), (112, 210), (80, 275), (112, 335), (80, 395),
+        (80, 455),
+    ]
 
-  for platform, position in zip(platforms, platform_positions):
-    platform.Create(position)
+    for platform, position in zip(platforms, platform_positions):
+        platform.Create(position)
 
-  action = proto.action_pb2.Action()
-  action.on_collision.scene_node_id.extend(['mario'])
-  action.on_collision.sprite_id.extend(['mario'])
-  action.on_collision.action.extend([dk.StopAnimationAction('dk_taunt')])
-  troll.execute(action.SerializeToString())
+    action = proto.action_pb2.Action()
+    action.on_collision.scene_node_id.extend(['mario'])
+    action.on_collision.sprite_id.extend(['mario'])
+    action.on_collision.action.extend([dk.StopAnimationAction('dk_taunt')])
+    action.on_collision.action.extend([dk.DestroyAction()])
+    troll.execute(action.SerializeToString())
--- data/scripts/sprites.py
+++ data/scripts/sprites.py
@@ -2,25 +2,25 @@ import troll_element
 
 
 class Mario(troll_element.TrollElement):
-  def __init__(self, id):
-      troll_element.TrollElement.__init__(self, id, 'mario')
+    def __init__(self, id):
+        troll_element.TrollElement.__init__(self, id, 'mario')
 
 
 class DonkeyKong(troll_element.TrollElement):
-  def __init__(self, id):
-      troll_element.TrollElement.__init__(self, id, 'dk')
+    def __init__(self, id):
+        troll_element.TrollElement.__init__(self, id, 'dk')
 
 
 class PlatformPiece(troll_element.TrollElement):
-  def __init__(self, id):
-      troll_element.TrollElement.__init__(self, id, 'platform')
+    def __init__(self, id):
+        troll_element.TrollElement.__init__(self, id, 'platform')
 
 
 class Platform(object):
-  def __init__(self, id, size):
-    self.pieces = [PlatformPiece(id + '_' + str(i)) for i in range(size)]
+    def __init__(self, id, size):
+        self.pieces = [PlatformPiece(id + '_' + str(i)) for i in range(size)]
 
-  def Create(self, position):
-    platform_width = 32
-    for i, piece in enumerate(self.pieces):
-      piece.Create(0, (position[0] + i * platform_width, position[1]))
+    def Create(self, position):
+        platform_width = 32
+        for i, piece in enumerate(self.pieces):
+            piece.Create(0, (position[0] + i * platform_width, position[1]))
