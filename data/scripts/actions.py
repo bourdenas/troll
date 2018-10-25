6f83c62d4817bac54ce241b4f945c8deedd5f763
Unknown <bourdenas@gmail.com>
2018-10-25 18:30:32 +0200

    Streamline python actions module removing packed flag.
    actions module now returns Action proto non-serialized and the caller has to call serialize before executing.
    This makes the interface better for client code writing Action effects for collisions.

--- data/scripts/actions.py
+++ data/scripts/actions.py
@@ -1,7 +1,7 @@
 import proto.action_pb2
 
 
-def Create(node_id, sprite_id, frame_index, position, packed=True):
+def Create(node_id, sprite_id, frame_index, position):
     action = proto.action_pb2.Action()
     action.create_scene_node.scene_node.id = node_id
     action.create_scene_node.scene_node.sprite_id = sprite_id
@@ -9,58 +9,58 @@ def Create(node_id, sprite_id, frame_index, position, packed=True):
     action.create_scene_node.scene_node.position.x = position[0]
     action.create_scene_node.scene_node.position.y = position[1]
     action.create_scene_node.scene_node.position.z = position[2]
-    return action.SerializeToString() if packed else action
+    return action
 
 
-def Destroy(node_id, packed=True):
+def Destroy(node_id):
     action = proto.action_pb2.Action()
     action.destroy_scene_node.scene_node.id = node_id
-    return action.SerializeToString() if packed else action
+    return action
 
 
-def PlayAnimation(node_id, script_id, packed=True):
+def PlayAnimation(node_id, script_id):
     action = proto.action_pb2.Action()
     action.play_animation_script.scene_node_id = node_id
     action.play_animation_script.script_id = script_id
-    return action.SerializeToString() if packed else action
+    return action
 
 
-def StopAnimation(node_id, script_id="", packed=True):
+def StopAnimation(node_id, script_id=""):
     action = proto.action_pb2.Action()
     action.stop_animation_script.scene_node_id = node_id
     action.stop_animation_script.script_id = script_id
-    return action.SerializeToString() if packed else action
+    return action
 
 
-def PauseAnimation(node_id, script_id="", packed=True):
+def PauseAnimation(node_id, script_id=""):
     action = proto.action_pb2.Action()
     action.pause_animation_script.scene_node_id = node_id
     action.pause_animation_script.script_id = script_id
-    return action.SerializeToString() if packed else action
+    return action
 
 
-def OnCollision(node_ids, sprite_ids, actions, packed=True):
+def OnCollision(node_ids, sprite_ids, actions):
     action = proto.action_pb2.Action()
     if node_ids:
         action.on_collision.scene_node_id.extend(node_ids)
     if sprite_ids:
         action.on_collision.sprite_id.extend(sprite_ids)
     action.on_collision.action.extend(actions)
-    return action.SerializeToString() if packed else action
+    return action
 
 
-def OnDetaching(node_ids, sprite_ids, actions, packed=True):
+def OnDetaching(node_ids, sprite_ids, actions):
     action = proto.action_pb2.Action()
     if node_ids:
         action.on_detaching.scene_node_id.extend(node_ids)
     if sprite_ids:
         action.on_detaching.sprite_id.extend(sprite_ids)
     action.on_detaching.action.extend(actions)
-    return action.SerializeToString() if packed else action
+    return action
 
 
-def Call(module, function, packed=True):
+def Call(module, function):
     action = proto.action_pb2.Action()
     action.call.module = module
     action.call.function = function
-    return action.SerializeToString() if packed else action
+    return action
--- data/scripts/troll_element.py
+++ data/scripts/troll_element.py
@@ -8,29 +8,35 @@ class Sprite:
         self.sprite_id = sprite_id
 
     def Create(self, position, frame_index=0):
-        troll.execute(actions.Create(
-            self.id, self.sprite_id, frame_index, position))
+        action = actions.Create(self.id, self.sprite_id, frame_index, position)
+        troll.execute(action.SerializeToString())
 
     def Destroy(self):
-        troll.execute(actions.Destroy(self.id))
+        action = actions.Destroy(self.id)
+        troll.execute(action.SerializeToString())
 
     def OnCollision(self, node_id='', sprite_id='', on_collide=[]):
-        troll.execute(actions.OnCollision(
+        action = actions.OnCollision(
             [self.id, node_id] if node_id else [self.id],
             [sprite_id] if sprite_id else [],
-            on_collide))
+            on_collide)
+        troll.execute(action.SerializeToString())
 
     def OnDetaching(self, node_id='', sprite_id='', on_detach=[]):
-        troll.execute(actions.OnDetaching(
+        action = actions.OnDetaching(
             [self.id, node_id] if node_id else [self.id],
             [sprite_id] if sprite_id else [],
-            on_detach))
+            on_detach)
+        troll.execute(action.SerializeToString())
 
     def PlayAnimation(self, script_id):
-        troll.execute(actions.PlayAnimation(self.id, script_id))
+        action = actions.PlayAnimation(self.id, script_id)
+        troll.execute(action.SerializeToString())
 
     def StopAnimation(self, script_id=""):
-        troll.execute(actions.StopAnimation(self.id, script_id))
+        action = actions.StopAnimation(self.id, script_id)
+        troll.execute(action.SerializeToString())
 
     def PauseAnimation(self, script_id=""):
-        troll.execute(actions.PauseAnimation(self.id, script_id))
+        action = actions.PauseAnimation(self.id, script_id)
+        troll.execute(action.SerializeToString())
