03e8a973637239c463567ef5388b8802f0c7e801
Unknown <bourdenas@gmail.com>
2018-10-31 12:49:02 +0100

    Delay-trigger handlers on events to avoid bad side-effects.

--- core/event-dispatcher.cc
+++ core/event-dispatcher.cc
@@ -1,5 +1,8 @@
 #include "core/event-dispatcher.h"
 
+#include <iterator>
+#include <vector>
+
 #include <absl/strings/str_join.h>
 
 namespace troll {
@@ -10,11 +13,18 @@ void EventDispatcher::Register(const std::string& event_id,
 }
 
 void EventDispatcher::Emit(const std::string& event_id) {
+  std::vector<EventHandler> handlers;
   for (auto it = event_registry_.lower_bound(event_id);
        it != event_registry_.upper_bound(event_id); ++it) {
-    it->second();
+    handlers.push_back(it->second);
   }
   event_registry_.erase(event_id);
+
+  // Delayed call of handlers because they cause side-effects that might
+  // register new events.
+  for (const auto& handler : handlers) {
+    handler();
+  }
 }
 
 }  // namespace troll
