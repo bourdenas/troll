225a8bd6ed26d37fa0b6e0816b4b6c6ad71a3ff0
Unknown <bourdenas@gmail.com>
2018-11-01 11:57:55 +0100

    Adapted topytroll.events module and added princess during intro.

--- data/scripts/sprites.py
+++ data/scripts/sprites.py
@@ -1,11 +1,16 @@
-import pytroll.sprite
 import proto.animation_pb2
-import troll
+import pytroll.events
+import pytroll.sprite
 
 
 class Mario(pytroll.sprite.Sprite):
     def __init__(self, id=''):
         pytroll.sprite.Sprite.__init__(self, id, 'mario')
+
+
+class Princess(pytroll.sprite.Sprite):
+    def __init__(self, id=''):
+        pytroll.sprite.Sprite.__init__(self, id, 'princess')
 
 
 class DonkeyKong(pytroll.sprite.Sprite):
@@ -28,22 +33,26 @@ class DonkeyKong(pytroll.sprite.Sprite):
         goto.delay = 18
         destroy_platforms.run_script.script_id = 'dk_jump'
 
-        def DestroyTopPlatform():
-            platforms[1].Collapse()
-        troll.on_event('.'.join((self.id, 'dk_landing', 'done')),
-                       lambda: DestroyTopPlatform())
-
-        def DestroyPlatform(dk, index):
-            platforms[index + 2].Collapse()
-            if index == 4:
-                return
-            dk.PlayAnimation(
-                'dk_jump', lambda: DestroyPlatform(dk, index + 1))
-        troll.on_event('.'.join((self.id, 'dk_jump', 'done')),
-                       lambda: DestroyPlatform(self, 0))
-
         script.animation.add().timer.delay = 300
         script.animation.add().run_script.script_id = 'dk_taunt'
+
+        def LandingHandler():
+            princess = Princess()
+            princess.Create((270, 50, -1), frame_index=1)
+            platforms[1].Collapse()
+        pytroll.events.OnEvent(
+            pytroll.events.AnimationDone(self.id, 'dk_landing'),
+            lambda: LandingHandler())
+
+        def DestroyPlatform(dk, index):
+            platforms[index].Collapse()
+            if index < 6:
+                dk.PlayAnimation(
+                    'dk_jump', lambda: DestroyPlatform(dk, index + 1))
+        pytroll.events.OnEvent(
+            pytroll.events.AnimationDone(self.id, 'dk_jump'),
+            lambda: DestroyPlatform(self, 2))
+
         return script
 
 
