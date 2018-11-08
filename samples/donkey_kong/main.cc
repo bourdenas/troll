3d7bf91374ebf2a006d306167747eb1d41a11d6f
Unknown <bourdenas@gmail.com>
2018-11-08 11:17:50 +0100

    Add donkey_kong project under samples.
    Started restructuring of sample projects.

---
+++ samples/donkey_kong/main.cc
@@ -0,0 +1,16 @@
+#include <glog/logging.h>
+
+#define SDL_MAIN_HANDLED
+#include "core/troll-core.h"
+
+int main(int argc, char *argv[]) {
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+  google::InitGoogleLogging(argv[0]);
+
+  troll::Core::Instance().Init();
+  troll::Core::Instance().Run();
+  troll::Core::Instance().CleanUp();
+
+  return 0;
+}
