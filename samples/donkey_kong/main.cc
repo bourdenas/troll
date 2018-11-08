f57bd3611653b6564930bedc826f1c61c710cbc0
Unknown <bourdenas@gmail.com>
2018-11-08 14:58:22 +0100

    Update donkey kong data base path.

--- samples/donkey_kong/main.cc
+++ samples/donkey_kong/main.cc
@@ -2,7 +2,7 @@
 #include "core/troll-core.h"
 
 int main(int argc, char *argv[]) {
-  troll::Core::Instance().Init(argv[0], "../data/");
+  troll::Core::Instance().Init(argv[0], "../samples/donkey_kong/data/");
   troll::Core::Instance().Run();
   troll::Core::Instance().CleanUp();
   return 0;
