62346f0303c4c7d86faa54968394c1033261dbcd
Unknown <bourdenas@gmail.com>
2019-02-20 22:21:45 +0100

    Replace googletest with catch2  for unit-tests.
    Convert a single module geometry_test to catch2.

--- core/geometry_test.cc
+++ core/geometry_test.cc
@@ -1,237 +1,233 @@
 #include "core/geometry.h"
 
-#include <gmock/gmock.h>
-#include <gtest/gtest.h>
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
 
 #include "troll-test/test-util.h"
 
 namespace troll {
 namespace geo {
 
-class GeometryTest : public testing::Test {
- protected:
-  void SetUp() override {}
-};
-
-TEST_F(GeometryTest, BoxContainsPoint) {
+TEST_CASE("BoxContainsPoint", "[GeometryTest]") {
   const auto box = ParseProto<Box>(R"(
     left: 10  top: 20  width: 30  height: 40)");
   const auto point = ParseProto<Vector>(R"(
     x: 15  y: 25)");
 
-  EXPECT_TRUE(Contains(box, point));
+  REQUIRE(Contains(box, point));
 }
 
-TEST_F(GeometryTest, BoxContainsPointIgnoresZAxis) {
+TEST_CASE("BoxContainsPointIgnoresZAxis", "[GeometryTest]") {
   const auto box = ParseProto<Box>(R"(
     left: 10  top: 20  width: 30  height: 40)");
 
   const auto front_point = ParseProto<Vector>(R"(
     x: 15  y: 25  z: 10)");
-  EXPECT_TRUE(Contains(box, front_point));
+  REQUIRE(Contains(box, front_point));
 
   const auto back_point = ParseProto<Vector>(R"(
     x: 15  y: 25  z: -10)");
-  EXPECT_TRUE(Contains(box, back_point));
+  REQUIRE(Contains(box, back_point));
 }
 
-TEST_F(GeometryTest, BoxDoesNotContainPoint) {
+TEST_CASE("BoxDoesNotContainPoint", "[GeometryTest]") {
   const auto box = ParseProto<Box>(R"(
     left: 10  top: 20  width: 30  height: 40)");
   const auto point = ParseProto<Vector>(R"(
     x: 5  y: 5)");
 
-  EXPECT_FALSE(Contains(box, point));
+  REQUIRE_FALSE(Contains(box, point));
 }
 
-TEST_F(GeometryTest, BoxDoesNotContainBorderPoints) {
+TEST_CASE("BoxDoesNotContainBorderPoints", "[GeometryTest]") {
   const auto box = ParseProto<Box>(R"(
     left: 10  top: 20  width: 30  height: 40)");
 
   const auto top_side_point = ParseProto<Vector>(R"(
     x: 15  y: 20)");
-  EXPECT_FALSE(Contains(box, top_side_point));
+  REQUIRE_FALSE(Contains(box, top_side_point));
 
   const auto top_left_corner_point = ParseProto<Vector>(R"(
     x: 10  y: 20)");
-  EXPECT_FALSE(Contains(box, top_left_corner_point));
+  REQUIRE_FALSE(Contains(box, top_left_corner_point));
 
   const auto right_side_point = ParseProto<Vector>(R"(
     x: 40  y: 35)");
-  EXPECT_FALSE(Contains(box, top_side_point));
+  REQUIRE_FALSE(Contains(box, top_side_point));
 
   const auto bottom_right_corner_point = ParseProto<Vector>(R"(
     x: 40  y: 60)");
-  EXPECT_FALSE(Contains(box, top_left_corner_point));
+  REQUIRE_FALSE(Contains(box, top_left_corner_point));
 }
 
-TEST_F(GeometryTest, CollidesBoxesMatchExactly) {
+TEST_CASE("CollidesBoxesMatchExactly", "[GeometryTest]") {
   const auto lhs = ParseProto<Box>(R"(
     left: 10  top: 10  width: 10  height: 20)");
 
-  EXPECT_TRUE(Collide(lhs, lhs));
+  REQUIRE(Collide(lhs, lhs));
 }
 
-TEST_F(GeometryTest, CollidesBoxesOverlapSignificantly) {
+TEST_CASE("CollidesBoxesOverlapSignificantly", "[GeometryTest]") {
   const auto lhs = ParseProto<Box>(R"(
     left: 10  top: 10  width: 10  height: 20)");
   const auto rhs = ParseProto<Box>(R"(
     left: 15  top: 15  width: 10  height: 10)");
 
-  EXPECT_TRUE(Collide(lhs, rhs));
-  EXPECT_TRUE(Collide(rhs, lhs));
+  REQUIRE(Collide(lhs, rhs));
+  REQUIRE(Collide(rhs, lhs));
 }
 
-TEST_F(GeometryTest, CollidesBoxesOverlapSlightly) {
+TEST_CASE("CollidesBoxesOverlapSlightly", "[GeometryTest]") {
   const auto lhs = ParseProto<Box>(R"(
     left: 10  top: 10  width: 10  height: 20)");
   const auto rhs = ParseProto<Box>(R"(
     left: 18  top: 28  width: 10  height: 10)");
 
-  EXPECT_TRUE(Collide(lhs, rhs));
-  EXPECT_TRUE(Collide(rhs, lhs));
+  REQUIRE(Collide(lhs, rhs));
+  REQUIRE(Collide(rhs, lhs));
 }
 
-TEST_F(GeometryTest, CollidesBoxesDoNotOverlap) {
+TEST_CASE("CollidesBoxesDoNotOverlap", "[GeometryTest]") {
   const auto lhs = ParseProto<Box>(R"(
     left: 0  top: 0  width: 10  height: 10)");
   const auto rhs = ParseProto<Box>(R"(
     left: 20  top: 5  width: 10  height: 10)");
 
-  EXPECT_FALSE(Collide(lhs, rhs));
-  EXPECT_FALSE(Collide(rhs, lhs));
+  REQUIRE_FALSE(Collide(lhs, rhs));
+  REQUIRE_FALSE(Collide(rhs, lhs));
 }
 
-TEST_F(GeometryTest, CollidesBoxesTouchOnSides) {
+TEST_CASE("CollidesBoxesTouchOnSides", "[GeometryTest]") {
   const auto lhs = ParseProto<Box>(R"(
     left: 0  top: 0  width: 10  height: 10)");
   const auto rhs = ParseProto<Box>(R"(
     left: 10  top: 0  width: 10  height: 10)");
 
-  EXPECT_FALSE(Collide(lhs, rhs));
-  EXPECT_FALSE(Collide(rhs, lhs));
+  REQUIRE_FALSE(Collide(lhs, rhs));
+  REQUIRE_FALSE(Collide(rhs, lhs));
 }
 
-TEST_F(GeometryTest, CollidesBoxesTouchOnCorners) {
+TEST_CASE("CollidesBoxesTouchOnCorners", "[GeometryTest]") {
   const auto lhs = ParseProto<Box>(R"(
     left: 0  top: 0  width: 10  height: 10)");
   const auto rhs = ParseProto<Box>(R"(
     left: 10  top: 10  width: 10  height: 10)");
 
-  EXPECT_FALSE(Collide(lhs, rhs));
-  EXPECT_FALSE(Collide(rhs, lhs));
+  REQUIRE_FALSE(Collide(lhs, rhs));
+  REQUIRE_FALSE(Collide(rhs, lhs));
 }
 
-TEST_F(GeometryTest, CollidesBoxesOneIncludesTheOther) {
+TEST_CASE("CollidesBoxesOneIncludesTheOther", "[GeometryTest]") {
   const auto lhs = ParseProto<Box>(R"(
     left: 0  top: 0  width: 20  height: 20)");
   const auto rhs = ParseProto<Box>(R"(
     left: 5  top: 5  width: 10  height: 10)");
 
-  EXPECT_TRUE(Collide(lhs, rhs));
-  EXPECT_TRUE(Collide(rhs, lhs));
+  REQUIRE(Collide(lhs, rhs));
+  REQUIRE(Collide(rhs, lhs));
 }
 
-TEST_F(GeometryTest, CollidesBoxesOneIncludesTheOtherTouchingsides) {
+TEST_CASE("CollidesBoxesOneIncludesTheOtherTouchingsides", "[GeometryTest]") {
   const auto lhs = ParseProto<Box>(R"(
     left: 0  top: 0  width: 20  height: 20)");
   const auto rhs = ParseProto<Box>(R"(
     left: 5  top: 5  width: 15  height: 15)");
 
-  EXPECT_TRUE(Collide(lhs, rhs));
-  EXPECT_TRUE(Collide(rhs, lhs));
+  REQUIRE(Collide(lhs, rhs));
+  REQUIRE(Collide(rhs, lhs));
 }
 
-TEST_F(GeometryTest, IntersectionBoxesMatchExactly) {
+TEST_CASE("IntersectionBoxesMatchExactly", "[GeometryTest]") {
   const auto lhs = ParseProto<Box>(R"(
     left: 10  top: 10  width: 10  height: 20)");
 
-  EXPECT_THAT(Intersection(lhs, lhs), EqualsProto(ParseProto<Box>(R"(
+  REQUIRE_THAT(Intersection(lhs, lhs), EqualsProto(ParseProto<Box>(R"(
     left: 10  top: 10  width: 10  height: 20)")));
 }
 
-TEST_F(GeometryTest, IntersectionBoxesOverlapSignificantly) {
+TEST_CASE("IntersectionBoxesOverlapSignificantly", "[GeometryTest]") {
   const auto lhs = ParseProto<Box>(R"(
     left: 10  top: 10  width: 10  height: 20)");
   const auto rhs = ParseProto<Box>(R"(
     left: 15  top: 15  width: 10  height: 10)");
 
-  EXPECT_THAT(Intersection(lhs, rhs), EqualsProto(ParseProto<Box>(R"(
+  REQUIRE_THAT(Intersection(lhs, rhs), EqualsProto(ParseProto<Box>(R"(
     left: 15  top: 15  width: 5  height: 10)")));
-  EXPECT_THAT(Intersection(rhs, lhs), EqualsProto(ParseProto<Box>(R"(
+  REQUIRE_THAT(Intersection(rhs, lhs), EqualsProto(ParseProto<Box>(R"(
     left: 15  top: 15  width: 5  height: 10)")));
 }
 
-TEST_F(GeometryTest, IntersectionBoxesOverlapSlightly) {
+TEST_CASE("IntersectionBoxesOverlapSlightly", "[GeometryTest]") {
   const auto lhs = ParseProto<Box>(R"(
     left: 10  top: 10  width: 10  height: 20)");
   const auto rhs = ParseProto<Box>(R"(
     left: 18  top: 28  width: 10  height: 10)");
 
-  EXPECT_THAT(Intersection(lhs, rhs), EqualsProto(ParseProto<Box>(R"(
+  REQUIRE_THAT(Intersection(lhs, rhs), EqualsProto(ParseProto<Box>(R"(
     left: 18  top: 28  width: 2  height: 2)")));
-  EXPECT_THAT(Intersection(rhs, lhs), EqualsProto(ParseProto<Box>(R"(
+  REQUIRE_THAT(Intersection(rhs, lhs), EqualsProto(ParseProto<Box>(R"(
     left: 18  top: 28  width: 2  height: 2)")));
 }
 
-TEST_F(GeometryTest, IntersectionBoxesDoNotOverlap) {
+TEST_CASE("IntersectionBoxesDoNotOverlap", "[GeometryTest]") {
   const auto lhs = ParseProto<Box>(R"(
     left: 0  top: 0  width: 10  height: 10)");
   const auto rhs = ParseProto<Box>(R"(
     left: 20  top: 5  width: 10  height: 10)");
 
   // When input boxes don't overlap, the resulting box is invalid.
-  EXPECT_THAT(Intersection(lhs, rhs), EqualsProto(ParseProto<Box>(R"(
+  REQUIRE_THAT(Intersection(lhs, rhs), EqualsProto(ParseProto<Box>(R"(
     left: 20  top: 5  width: -10  height: 5)")));
-  EXPECT_THAT(Intersection(rhs, lhs), EqualsProto(ParseProto<Box>(R"(
+  REQUIRE_THAT(Intersection(rhs, lhs), EqualsProto(ParseProto<Box>(R"(
     left: 20  top: 5  width: -10  height: 5)")));
 }
 
-TEST_F(GeometryTest, IntersectionBoxesTouchOnSides) {
+TEST_CASE("IntersectionBoxesTouchOnSides", "[GeometryTest]") {
   const auto lhs = ParseProto<Box>(R"(
     left: 0  top: 0  width: 10  height: 10)");
   const auto rhs = ParseProto<Box>(R"(
     left: 10  top: 0  width: 10  height: 10)");
 
-  EXPECT_THAT(Intersection(lhs, rhs), EqualsProto(ParseProto<Box>(R"(
+  REQUIRE_THAT(Intersection(lhs, rhs), EqualsProto(ParseProto<Box>(R"(
     left: 10  top: 0  width: 0  height: 10)")));
-  EXPECT_THAT(Intersection(rhs, lhs), EqualsProto(ParseProto<Box>(R"(
+  REQUIRE_THAT(Intersection(rhs, lhs), EqualsProto(ParseProto<Box>(R"(
     left: 10  top: 0  width: 0  height: 10)")));
 }
 
-TEST_F(GeometryTest, IntersectionBoxesTouchOnCorners) {
+TEST_CASE("IntersectionBoxesTouchOnCorners", "[GeometryTest]") {
   const auto lhs = ParseProto<Box>(R"(
     left: 0  top: 0  width: 10  height: 10)");
   const auto rhs = ParseProto<Box>(R"(
     left: 10  top: 10  width: 10  height: 10)");
 
-  EXPECT_THAT(Intersection(lhs, rhs), EqualsProto(ParseProto<Box>(R"(
+  REQUIRE_THAT(Intersection(lhs, rhs), EqualsProto(ParseProto<Box>(R"(
     left: 10  top: 10  width: 0  height: 0)")));
-  EXPECT_THAT(Intersection(rhs, lhs), EqualsProto(ParseProto<Box>(R"(
+  REQUIRE_THAT(Intersection(rhs, lhs), EqualsProto(ParseProto<Box>(R"(
     left: 10  top: 10  width: 0  height: 0)")));
 }
 
-TEST_F(GeometryTest, IntersectionBoxesOneIncludesTheOther) {
+TEST_CASE("IntersectionBoxesOneIncludesTheOther", "[GeometryTest]") {
   const auto lhs = ParseProto<Box>(R"(
     left: 0  top: 0  width: 20  height: 20)");
   const auto rhs = ParseProto<Box>(R"(
     left: 5  top: 5  width: 10  height: 10)");
 
-  EXPECT_THAT(Intersection(lhs, rhs), EqualsProto(ParseProto<Box>(R"(
+  REQUIRE_THAT(Intersection(lhs, rhs), EqualsProto(ParseProto<Box>(R"(
     left: 5  top: 5  width: 10  height: 10)")));
-  EXPECT_THAT(Intersection(rhs, lhs), EqualsProto(ParseProto<Box>(R"(
+  REQUIRE_THAT(Intersection(rhs, lhs), EqualsProto(ParseProto<Box>(R"(
     left: 5  top: 5  width: 10  height: 10)")));
 }
 
-TEST_F(GeometryTest, IntersectionBoxesOneIncludesTheOtherTouchingsides) {
+TEST_CASE("IntersectionBoxesOneIncludesTheOtherTouchingsides",
+          "[GeometryTest]") {
   const auto lhs = ParseProto<Box>(R"(
     left: 0  top: 0  width: 20  height: 20)");
   const auto rhs = ParseProto<Box>(R"(
     left: 5  top: 5  width: 15  height: 15)");
 
-  EXPECT_THAT(Intersection(lhs, rhs), EqualsProto(ParseProto<Box>(R"(
+  REQUIRE_THAT(Intersection(lhs, rhs), EqualsProto(ParseProto<Box>(R"(
     left: 5  top: 5  width: 15  height: 15)")));
-  EXPECT_THAT(Intersection(rhs, lhs), EqualsProto(ParseProto<Box>(R"(
+  REQUIRE_THAT(Intersection(rhs, lhs), EqualsProto(ParseProto<Box>(R"(
     left: 5  top: 5  width: 15  height: 15)")));
 }
 
