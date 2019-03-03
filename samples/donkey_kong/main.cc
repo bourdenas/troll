768def780661bbb205fd6156e94af3eff91e1088
Unknown <bourdenas@gmail.com>
2019-03-03 16:23:48 +0100

    Update DonkeyKong sample game to start scene.

--- samples/donkey_kong/main.cc
+++ samples/donkey_kong/main.cc
@@ -2,10 +2,12 @@
 
 #include "core/resource-manager.h"
 #include "core/troll-core.h"
+#include "scripting/script-manager.h"
 
 int main(int argc, char *argv[]) {
   troll::TrollCore core;
   core.Init(argv[0], "../samples/donkey_kong/data/");
+  core.script_manager()->CreateScene("dk.intro", "IntroScene");
   core.Run();
 
   return 0;
