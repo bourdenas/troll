d9d834fe28069f42af41da061e6830fbc7859d76
Unknown <bourdenas@gmail.com>
2019-03-09 21:22:37 +0100

    Add dart_utils for dealing with data conversion across languages.

--- dart_troll/dart_troll.cc
+++ dart_troll/dart_troll.cc
@@ -3,51 +3,47 @@
 #include <dart_api.h>
 
 #include "core/troll-core.h"
+#include "dart_troll/dart_utils.h"
 
+namespace troll {
 Dart_NativeFunction ResolveName(Dart_Handle name, int argc,
                                 bool* auto_setup_scope);
+}  // namespace troll
 
+// Dart native extension entrypoint.
 DART_EXPORT Dart_Handle dart_troll_Init(Dart_Handle parent_library) {
   if (Dart_IsError(parent_library)) return parent_library;
 
   Dart_Handle result_code =
-      Dart_SetNativeResolver(parent_library, ResolveName, nullptr);
+      Dart_SetNativeResolver(parent_library, troll::ResolveName, nullptr);
   if (Dart_IsError(result_code)) return result_code;
 
   return Dart_Null();
 }
 
-Dart_Handle HandleError(Dart_Handle handle) {
-  if (Dart_IsError(handle)) Dart_PropagateError(handle);
-  return handle;
-}
+namespace troll {
 
-troll::TrollCore* core;
+TrollCore* core;
 
+// Sets up troll-core.
 void NativeInit(Dart_NativeArguments arguments) {
   const Dart_Handle name = HandleError(Dart_GetNativeArgument(arguments, 0));
-  const char* cstr_name;
-  HandleError(Dart_StringToCString(name, &cstr_name));
-
   const Dart_Handle resource_path =
       HandleError(Dart_GetNativeArgument(arguments, 1));
-  const char* cstr_resource_path;
-  HandleError(Dart_StringToCString(resource_path, &cstr_resource_path));
 
-  core = new troll::TrollCore;
-  core->Init(cstr_name, cstr_resource_path, nullptr);
+  core = new TrollCore;
+  core->Init(DownloadString(name), DownloadString(resource_path), nullptr);
 }
 
+// Start game engine execution.
 void NativeRun(Dart_NativeArguments arguments) { core->Run(); }
 
+// Resolves Darts calls to native functions by name.
 Dart_NativeFunction ResolveName(Dart_Handle name, int argc,
                                 bool* auto_setup_scope) {
   if (!Dart_IsString(name)) return nullptr;
 
-  const char* cname;
-  HandleError(Dart_StringToCString(name, &cname));
-  const std::string_view func_name = cname;
-
+  const auto func_name = DownloadString(name);
   if (func_name == "NativeInit") {
     return NativeInit;
   }
@@ -57,3 +53,5 @@ Dart_NativeFunction ResolveName(Dart_Handle name, int argc,
 
   return nullptr;
 }
+
+}  // namespace troll
