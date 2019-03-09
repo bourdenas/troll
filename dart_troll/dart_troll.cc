f0de79df35764d76d43970e819dcb2da27ab8bfe
Unknown <bourdenas@gmail.com>
2019-03-09 23:17:18 +0100

    Implement Execute() call for Dart.

--- dart_troll/dart_troll.cc
+++ dart_troll/dart_troll.cc
@@ -38,6 +38,14 @@ void NativeInit(Dart_NativeArguments arguments) {
 // Start game engine execution.
 void NativeRun(Dart_NativeArguments arguments) { core->Run(); }
 
+// Execute an action through troll's executors system.
+void NativeExecute(Dart_NativeArguments arguments) {
+  const Dart_Handle action_buffer =
+      HandleError(Dart_GetNativeArgument(arguments, 0));
+  const auto action = DownloadProtoValue<Action>(action_buffer);
+  core->action_manager()->Execute(action);
+}
+
 // Resolves Darts calls to native functions by name.
 Dart_NativeFunction ResolveName(Dart_Handle name, int argc,
                                 bool* auto_setup_scope) {
@@ -49,6 +57,9 @@ Dart_NativeFunction ResolveName(Dart_Handle name, int argc,
   }
   if (func_name == "NativeRun") {
     return NativeRun;
+  }
+  if (func_name == "NativeExecute") {
+    return NativeExecute;
   }
 
   return nullptr;
--- dart_troll/lib/dart_troll.dart
+++ dart_troll/lib/dart_troll.dart
@@ -1,7 +1,10 @@
 library dart_troll;
 
+import 'dart:typed_data';
 import 'dart-ext:dart_troll';
 
 void init(String name, String resourcePath) native "NativeInit";
 
-void run(Function f) native "NativeRun";
+void run() native "NativeRun";
+
+void execute(Uint8List action) native "NativeExecute";
