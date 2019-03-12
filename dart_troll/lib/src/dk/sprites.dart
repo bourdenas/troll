3a58a69c0ddae03e02be6faf50c461a35085a396
Unknown <bourdenas@gmail.com>
2019-03-12 22:13:10 +0100

    Implement Platform.collapse() and add Mario & Princess sprites.

--- dart_troll/lib/src/dk/sprites.dart
+++ dart_troll/lib/src/dk/sprites.dart
@@ -1,7 +1,15 @@
 import 'package:dart_troll/src/core/sprite.dart';
 
+class Mario extends Sprite {
+  Mario() : super(null, 'mario');
+}
+
 class DonkeyKong extends Sprite {
   DonkeyKong() : super(null, 'dk');
+}
+
+class Princess extends Sprite {
+  Princess() : super(null, 'princess');
 }
 
 class PlatformPiece extends Sprite {
@@ -9,22 +17,44 @@ class PlatformPiece extends Sprite {
 }
 
 class Platform {
+  int index;
   int size;
   List<PlatformPiece> pieces;
 
   static int platformWidth = 32;
 
-  Platform(this.size) {
+  Platform(this.index, this.size) {
     pieces = List<PlatformPiece>.generate(size, (index) {
       return PlatformPiece();
     });
   }
 
   void create(List<int> position) {
-    pieces.forEach((piece) {
+    for (final piece in pieces) {
       position[0] += platformWidth;
       piece.create(position, 0);
-    });
+    }
+  }
+
+  void collapse() {
+    if (index == 1) {
+      for (int i = 0; i < 4; ++i) {
+        pieces[pieces.length - i - 1].move([0, 4 - i]);
+      }
+    } else if (index == 6) {
+      for (int i = 0; i < 7; ++i) {
+        pieces[pieces.length - i - 1].move([0, i]);
+        pieces[i].move([0, 7]);
+      }
+    } else if (index % 2 == 0) {
+      pieces.asMap().forEach((i, piece) {
+        piece.move([0, pieces.length - i - 1]);
+      });
+    } else if (index % 2 == 1) {
+      pieces.asMap().forEach((i, piece) {
+        piece.move([0, i]);
+      });
+    }
   }
 }
 
