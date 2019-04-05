e8157a4bc0c7c3b050ba309ec53814ab3d855f53
Unknown <bourdenas@gmail.com>
2019-04-05 22:52:34 +0200

    Fix function arguments to Dart style.

--- dart_troll/lib/dart_troll.dart
+++ dart_troll/lib/dart_troll.dart
@@ -11,13 +11,12 @@ void execute(Uint8List action) native "NativeExecute";
 
 Uint8List eval(Uint8List query) native "NativeEval";
 
-int registerEventHandler(String event_id, Function handler,
+int registerEventHandler(String eventId, Function handler,
     {bool permanent = false}) native "NativeRegisterEventHandler";
 
-int unregisterEventHandler(String event_id, int handler_id)
+int unregisterEventHandler(String eventId, int handlerId)
     native "NativeUnregisterEventHandler";
 
 int registerInputHandler(Function handler) native "NativeRegisterInputHandler";
 
-int unregisterInputHandler(int handler_id)
-    native "NativeUnregisterInputHandler";
+int unregisterInputHandler(int handlerId) native "NativeUnregisterInputHandler";
